5cfbefe185a3505af05398c0d0c486e609052fbc
Lucas <lucas@Lucass-MacBook-Air.local>
2021-02-21 14:51:19 -0800

    time checker for day and hour

---
+++ scheduler.py
@@ -0,0 +1,17 @@
+import datetime
+hour_play_begin = 15 
+hour_play_end = 21
+
+def time_to_play():
+    hour = int(datetime.datetime.now().strftime("%H"))
+    day = datetime.datetime.now().strftime("%a")
+    if day == 'Fri': 
+        if hour >= 15:
+            return True
+    if day == 'Sat':
+        return True
+    if day == 'Sun':
+        if hour <= 21:
+            return True
+
+    
